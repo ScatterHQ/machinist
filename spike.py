537acd26118199e23e3b2afd11c055e7812e5216
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-11-01 10:22:37 -0500

    matplotlib is probably a bust; think about just polishing graphviz version a bit

--- spike.py
+++ spike.py
@@ -22,12 +22,14 @@ def add_edges(graph, table):
         for (input_symbol, transition) in transitions.items():
             graph.add_edge(
                 from_state.name, transition.nextState.name,
-                dict(label=input_symbol.name),
+                label=input_symbol.name,
             )
     return graph
 
 
 def render_graphviz(graph_name, graph):
+    graph = graph.copy()
+    # XXX Set colors on nodes and labels so you can tell them apart in the visualization
     a = to_agraph(graph)
     a.layout("dot")
     a.draw(graph_name + ".machinist.ps")
@@ -37,7 +39,7 @@ def render_matplotlib(graph_name, graph):
     from matplotlib import pyplot as plt
     from networkx import spring_layout, graphviz_layout, draw_networkx, draw_spring, get_edge_attributes
     plt.rcParams['text.usetex'] = False
-    plt.figure(figsize=(24, 24))
+    plt.figure(figsize=(16, 16))
     pos = spring_layout(graph)
 
     # Yea, just a giant heap of global mutable state.  Why not, anyway?
@@ -62,6 +64,9 @@ def render_matplotlib(graph_name, graph):
     draw_networkx_edge_labels(
         graph,
         pos,
+        # This is where things screw up.  get_edge_attributes returns a
+        # different data structure for MultiDiGraphs than for DiGraphs.
+        # draw_networkx_edge_labels can't deal with the difference.
         edge_labels=get_edge_attributes(graph, "label"),
     )
     # draw_spring(graph)
@@ -84,6 +89,9 @@ renderers = {
 def main():
     table = namedAny(argv[1])
 
+    # matplotlib renderer has problems with MultiDiGraph.  State machine may
+    # well have multiple transitions between a particular state pair, though
+    # ... Use a DiGraph so something gets rendered, even if it's wrong (!!!)
     g = add_states(DiGraph(), table)
     g = add_edges(g, table)
 
